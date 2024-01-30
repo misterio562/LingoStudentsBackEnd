bf5b094fa7c74bad3c74280ad18379cf48158960
Juan Ayala <misterio562@gmail.com>
2024-01-29 20:46:31 -0500

    refactor: :recycle: change createConnection to createPool

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,13 @@
-import mysql from "promise-mysql";
+import mysql from "mysql2";
 import config from "./../config.js";
 
-const connection = mysql.createConnection({
+const connection = mysql.createPool({
   host: config.host,
   user: config.user,
   password: config.password,
   database: config.database,
 });
+
 const getConnection = () => {
   return connection;
 };
